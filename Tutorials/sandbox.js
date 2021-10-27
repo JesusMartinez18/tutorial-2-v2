f48adc36143f7d07046dda350e44c72c41c6868c
JesusMartinez18 <62106052+JesusMartinez18@users.noreply.github.com>
2021-10-27 11:39:41 -0400

    Add files via upload

---
+++ Tutorials/sandbox.js
@@ -0,0 +1,64 @@
+// /*console.log(1);
+// console.log(2);
+// let age = 25;
+// let year = 2019;
+// // example of comments!!!!!
+// /* my name
+// is jesus
+// and this
+// is
+// a comment example */
+// console.log(age, year);
+//
+// age= 30;
+// console.log(age);
+//
+//
+//
+// var score= 75;
+// console.log(score);
+//
+// //strings
+// console.log('hello, world');
+//
+// let email = 'mario@thenetninja.co.uk';
+// console.log(email);
+//
+// // string concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
+//
+// let fullName = firstName + '' + lastName;
+// console.log(fullName);
+//
+// //getting characters
+// console.log(fullName[2]);
+//
+// // string length
+// console.log(fullName.length);
+//
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+//
+// let index = email.indexOf('@');
+// console.log(index);
+//
+// let email = 'mario@thenetninja.co.uk';
+//
+//  let result = email.lastIndexOf('n');
+//
+//  console.log(result);
+
+// type conversion
+let score = '100';
+
+// score = Numnber(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+//let result = Number('hello');
+let result = string(50);
+
+console.log(result, typeof result);
